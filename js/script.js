a141095cb2498af0268780b07440ab0ac239053d
Dillon <dillonoconnor55@gmail.com>
2024-05-27 16:45:04 +0100

    added employment.html employment.css added new script to script.js and 3 images for employment page.

--- js/script.js
+++ js/script.js
@@ -6,3 +6,21 @@ function toggleAccordion(id) {
     content.style.display = "block";
   }
 }
+
+document.addEventListener('DOMContentLoaded', function() {
+  const buttons = document.querySelectorAll('.read-more-btn');
+
+  buttons.forEach(button => {
+    button.addEventListener('click', function() {
+      const collapsibleContent = this.nextElementSibling;
+
+      if (collapsibleContent.style.display === 'block') {
+        collapsibleContent.style.display = 'none';
+        this.textContent = 'Read More';
+      } else {
+        collapsibleContent.style.display = 'block';
+        this.textContent = 'Read Less';
+      }
+    });
+  });
+});
